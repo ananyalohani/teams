51fcd6b618438ac9386b274988c6937cea2820a2
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-06-18 15:51:53 +0530

    Add custom 404 page

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='bg-gray-800 flex items-center justify-center border-t border-gray-600'>
-      <p className='text-gray-400 p-6'>
+    <footer className='bg-gray-800 flex items-center justify-center border-t border-gray-600 h-16'>
+      <p className='text-gray-400'>
         Developed by{' '}
         <a
           href='https://lohani.dev/'
---
+++ pages/404.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '@/components/layout';
+
+function Custom404() {
+  return (
+    <Layout title='Not Found'>
+      <div
+        className='flex flex-col items-center justify-center space-y-5'
+        style={{ height: 'calc(100vh - 10rem)' }}
+      >
+        <img
+          src='/images/page_not_found.png'
+          className='max-w-lg'
+          alt='page not found'
+        />
+        <div className='text-3xl text-light flex flex-row space-x-3'>
+          <p className='text-red-400 font-bold'>404</p>
+          <div style={{ width: 1 }} className='bg-gray-200 opacity-40'></div>
+          <p>The page you're looking for does not exist.</p>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Custom404;
